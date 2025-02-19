ee69034c0f1c32c8af9e87366df82e6c0d48c66e
Thiago Leite <thiagofmleite@gmail.com>
2025-02-19 11:19:22 -0300

    Implement Wallet entity with Mongoose schema and update Wallets service methods

--- src/wallets/entities/wallet.entity.ts
+++ src/wallets/entities/wallet.entity.ts
@@ -1,1 +1,18 @@
-export class Wallet {}
+import crypto from 'node:crypto';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type WalletDocument = HydratedDocument<Wallet>;
+
+@Schema({
+  timestamps: true,
+})
+export class Wallet {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export const WalletSchema = SchemaFactory.createForClass(Wallet);
--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
+import { WalletSchema } from './entities/wallet.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Wallet',
+        schema: WalletSchema,
+      },
+    ]),
+  ],
   controllers: [WalletsController],
   providers: [WalletsService],
 })
