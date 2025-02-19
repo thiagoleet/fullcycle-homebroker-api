ae5dcf23c6ccbd45879ead624cc02c00562bb286
Thiago Leite <thiagofmleite@gmail.com>
2025-02-19 11:13:31 -0300

    Integrate Mongoose module and add Wallets module to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { WalletsModule } from './wallets/wallets.module';
 @Module({
-  imports: [AssetsModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root@localhost:27017/nest?authSource=admin&directConnection=true',
+    ),
+    AssetsModule,
+    WalletsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
