890c0f773c017ab4b37f759d0205218b0c8837b5
Thiago Leite <thiagofmleite@gmail.com>
2025-02-19 10:14:18 -0300

    Define Asset entity with Mongoose schema and properties

--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,1 +1,30 @@
-export class Asset {}
+import crypto from 'node:crypto';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type AssetDocument = HydratedDocument<Asset>;
+
+@Schema({
+  timestamps: true,
+})
+export class Asset {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  symbol: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  price: number;
+
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export const AssetSchema = SchemaFactory.createForClass(Asset);
